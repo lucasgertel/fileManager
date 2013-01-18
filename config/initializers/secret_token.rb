49b80f9f523f227b801d2390f9edc8da6622e2dd
lucasgertel <lucas.gertel@realwebtrade.com.br>
2013-01-18 15:10:50 -0200

    Project creation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FileManager::Application.config.secret_token = 'fe9b3f452aa6f937780aee188520c47ccca5d60c7a3055d24238db650cc52de9d9fe3b00b4b08cd22b1e1039c0a1ec17b4d94f8f4b9e74bef546fb3736d88d44'
